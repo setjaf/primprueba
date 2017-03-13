7f702aa579162d35e43b8798747273ca5452f4e9
Set <setjafet@gmail.com>
2017-03-12 21:15:39 -0600

    cambio server5

--- deploy.cmd
+++ deploy.cmd
@@ -102,8 +102,6 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
   call :ExecuteCmd !NPM_CMD! install --production
   IF !ERRORLEVEL! NEQ 0 goto error
-  call :ExecuteCmd !NPM_CMD! run now-start
-  IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
 
